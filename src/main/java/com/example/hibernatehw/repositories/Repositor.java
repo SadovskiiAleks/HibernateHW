98fe0fdaeba4710b4a83115645654c53cf7bb8a6
SadovskiiAleks <alekssadovskij@yandex.ru>
2023-10-08 18:27:25 +0300

    add DAO HW

---
+++ src/main/java/com/example/hibernatehw/repositories/Repositor.java
@@ -0,0 +1,7 @@
+package com.example.hibernatehw.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface Repositor {
+}
---
+++ src/main/java/com/example/hibernatehw/repositories/entity/Persons.java
@@ -0,0 +1,32 @@
+package com.example.hibernatehw.repositories.entity;
+
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
+@Table( name = "Persons",
+        catalog = "test",
+        schema = "public")
+public class Persons {
+
+
+//    первичным ключом будет сочетание name, surname, age.
+    @EmbeddedId
+    private User user;
+
+    private int phone_number;
+    private String city_of_living;
+
+
+    public Persons() {
+
+    }
+}
